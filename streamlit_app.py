02e199b4f15cbc6ae78a1e587e6bb89bfe05f0cc
Soujatya Das <soujatya548@gmail.com>
2025-01-28 14:29:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if uploaded_file is not None:
   template = '''Write a very concise, well-explained, point-wise, short summary of the following text. Provide good and user-acceptable response.
   HR: '{text}.
   Create section-wise summary. Also, mention what the document uploaded is aimed at doing, as in its purpose.
-  If applicable, display the respondents name as well just after the purpose. Also, at the end mention the key findings in points'
+  If applicable, display the involved parties name as well just after the purpose. Also, at the end mention the key findings in points'
   '''
 
   prompt = PromptTemplate(
