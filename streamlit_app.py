12cb6acacaa59b8deb03a628e97be03387e53df1
Soujatya Das <soujatya548@gmail.com>
2025-01-31 09:45:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ if uploaded_file is not None:
   llm = ChatGroq(groq_api_key = api_key, model_name = 'llama3-70b-8192', temperature = 0.2, top_p = 0.2)
 
   template = '''Write a very concise, well-explained, point-wise, short summary of the following text. Provide good and user-acceptable response.
-  HR: '{text}.
+  '{text}
   Create section-wise summary. Also, mention what the document uploaded is aimed at doing, as in its purpose.
   If applicable, display the involved parties name as well just after the purpose. Also, at the end mention the key findings in points'
   '''
